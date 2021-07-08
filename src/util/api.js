0f0abaa689233ea7989ca53aaf9d8dd73bf12617
unknown <nesad.omeragich27@gmail.com>
2021-07-08 04:01:59 +0200

    Feat: add util function for posting data

---
+++ src/util/api.js
@@ -0,0 +1,12 @@
+export const postSignUpData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    credentials: 'same-origin',
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
