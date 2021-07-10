493018cadf7abda9bc8ad409eab22baa3c3b7316
unknown <nesad.omeragich27@gmail.com>
2021-07-10 19:29:59 +0200

    add protected route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { PrivateRoute } from './util/PrivateRoute';
 
 import Signup from './features/auth/components/SignUp/SignUp';
 import Login from './features/auth/components/LogIn/LogIn';
+import Main from './features/posts/components/Main';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={Login} />
-      <Route path='/signup' component={Signup} />
+      <Route exact path='/signup' component={Signup} />
+      <PrivateRoute exact path='/main'>
+        <Main />
+      </PrivateRoute>
     </Router>
   );
 }
