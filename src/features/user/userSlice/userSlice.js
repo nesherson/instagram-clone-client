6b8f7850f0f6ac88892bdd0d1dd96a279f48624e
unknown <nesad.omeragich27@gmail.com>
2021-07-12 02:26:37 +0200

    Feat: add user slice

--- src/features/user/userSlice/userSlice.js
+++ src/features/user/userSlice/userSlice.js
@@ -14,7 +14,6 @@ export const signupUser = createAsyncThunk(
       });
 
       let data = await response.json();
-      console.log('userSlice/signupUser/data: ', data);
 
       if (response.status === 200) {
         localStorage.setItem('token', data.token);
@@ -23,7 +22,6 @@ export const signupUser = createAsyncThunk(
         return thunkAPI.rejectWithValue(data);
       }
     } catch (err) {
-      console.log('userSlice/signupUser/catch/error: ', err.response.data);
       return thunkAPI.rejectWithValue(err.response.data);
     }
   }
@@ -43,7 +41,6 @@ export const loginUser = createAsyncThunk(
       });
 
       let data = await response.json();
-      console.log('userSlice/loginUser/data: ', data);
 
       if (response.status === 200) {
         localStorage.setItem('token', data.token);
@@ -52,7 +49,32 @@ export const loginUser = createAsyncThunk(
         return thunkAPI.rejectWithValue(data);
       }
     } catch (err) {
-      console.log('userSlice/signupUser/catch/error: ', err.response.data);
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const fetchUserByToken = createAsyncThunk(
+  'user/fetchUserByToken',
+  async (token, thunkAPI) => {
+    try {
+      const response = await fetch('http://localhost:5000/auth/user', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'x-auth-token': token,
+        },
+      });
+
+      let data = await response.json();
+
+      if (response.status === 200) {
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (err) {
       return thunkAPI.rejectWithValue(err.response.data);
     }
   }
@@ -62,6 +84,7 @@ const initialState = {
   email: '',
   fullname: '',
   username: '',
+  profileImg: '',
   isFetching: false,
   isSuccess: false,
   isError: false,
@@ -101,6 +124,24 @@ const userSlice = createSlice({
       state.isFetching = true;
     },
     [loginUser.rejected]: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = false;
+      state.isError = true;
+      state.errorMessage = action.payload.message;
+    },
+    [fetchUserByToken.fulfilled]: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+      state.isError = false;
+      state.email = action.payload.user.email;
+      state.username = action.payload.user.username;
+      state.fullname = action.payload.user.fullname;
+      state.profileImg = action.payload.user.profileImg;
+    },
+    [fetchUserByToken.pending]: (state, action) => {
+      state.isFetching = true;
+    },
+    [fetchUserByToken.rejected]: (state, action) => {
       state.isFetching = false;
       state.isSuccess = false;
       state.isError = true;
--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import signUpReducer from '../features/auth/authSlice/signUpSlice';
 import logInReducer from '../features/auth/authSlice/logInSlice';
 import userReducer from '../features/user/userSlice/userSlice';
+import newPostReducer from '../features/posts/postsSlice/newPostSlice';
 
 export const store = configureStore({
   reducer: {
     signUp: signUpReducer,
     logIn: logInReducer,
     user: userReducer,
+    newPost: newPostReducer,
   },
 });
