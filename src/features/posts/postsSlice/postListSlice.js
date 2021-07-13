51c4b21342c5dd8946053810e1b2c209a01c4e21
unknown <nesad.omeragich27@gmail.com>
2021-07-13 04:19:21 +0200

    Feat: add post list slice

---
+++ src/features/posts/postsSlice/postListSlice.js
@@ -0,0 +1,61 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchPosts = createAsyncThunk(
+  'postList/fetchPosts',
+  async (token, thunkAPI) => {
+    try {
+      const response = await fetch('http://localhost:5000/post/posts', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'x-auth-token': token,
+        },
+      });
+
+      let data = await response.json();
+
+      if (response.status === 200) {
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err);
+    }
+  }
+);
+
+const initialState = {
+  posts: [],
+  isFetching: false,
+  isSuccess: false,
+  isError: false,
+  errorMessage: '',
+};
+
+const postListSlice = createSlice({
+  name: 'postList',
+  initialState,
+  extraReducers: {
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+      state.isError = false;
+      state.posts = action.payload.posts;
+    },
+    [fetchPosts.pending]: (state, action) => {
+      state.isFetching = true;
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = false;
+      state.isError = true;
+      state.errorMessage = action.payload.message;
+    },
+  },
+});
+
+export const selectPosts = (state) => state.postList.posts;
+
+export default postListSlice.reducer;
