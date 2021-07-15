ce28bb7ba968ff1f5668b0db84c97bc1429e2972
unknown <nesad.omeragich27@gmail.com>
2021-07-15 15:58:06 +0200

    Feat: add private route to user profile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { PrivateRoute } from './util/PrivateRoute';
 
-import Signup from './features/auth/components/SignUp/SignUp';
-import Login from './features/auth/components/LogIn/LogIn';
+import Signup from './features/auth/components/Signup/Signup';
+import Login from './features/auth/components/Login/Login';
 import Feed from './features/posts/components/Feed';
 import UserProfile from './features/user/components/userProfile';
 
@@ -10,11 +10,12 @@ function App() {
   return (
     <Router>
       <Route exact path='/' component={Login} />
-      <Route exact path='/signup' component={Signup} />
-      <PrivateRoute exact path='/feed'>
+      <Route path='/signup' component={Signup} />
+      <PrivateRoute path='/feed'>
         <Feed />
+        <UserProfile />
       </PrivateRoute>
-      <PrivateRoute exact path='/:id'>
+      <PrivateRoute path='/profile/:id'>
         <UserProfile />
       </PrivateRoute>
     </Router>
