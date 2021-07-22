95e8b567573c0b9a896d8d72e9753e02305e9786
nesherson <nesad.omeragich27@gmail.com>
2021-07-22 22:02:02 +0200

    Refactor: change in routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { PrivateRoute } from './util/PrivateRoute';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { PrivateRoute } from "./util/PrivateRoute";
 
-import Signup from './features/auth/components/Signup/Signup';
-import Login from './features/auth/components/Login/Login';
-import Feed from './features/posts/components/Feed';
-import UserProfile from './features/user/components/userProfile';
+import Signup from "./features/auth/components/Signup/Signup";
+import Login from "./features/auth/components/Login/Login";
+import Feed from "./features/posts/components/Feed";
+import UserProfile from "./features/user/components/userProfile";
 
 function App() {
+  const token = JSON.parse(localStorage.getItem("userData"));
+
   return (
     <Router>
-      <Route exact path='/' component={Login} />
-      <Route path='/signup' component={Signup} />
-      <PrivateRoute path='/feed'>
+      <PrivateRoute exact path="/feed">
         <Feed />
-        <UserProfile />
       </PrivateRoute>
-      <PrivateRoute path='/profile/:id'>
-        <UserProfile />
+      <PrivateRoute path="/profile/:id">
+        <UserProfile/>
       </PrivateRoute>
+
+     { !token ?
+       <>
+        <Route exact path="/" component={Login} />
+        <Route path="/signup" component={Signup} />
+       </> : <Redirect to='/feed' />
+     }
     </Router>
   );
 }
