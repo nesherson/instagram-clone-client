9ac2a5d7da044da617e569f419d09ef05ee194a7
nesherson <nesad.omeragich27@gmail.com>
2021-07-29 23:46:26 +0200

    Feat: add regExp for new post url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 const EMAIL_REGEXP = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-
-export {EMAIL_REGEXP};
+const URL_REGEXP = new RegExp(/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/);
+export {EMAIL_REGEXP, URL_REGEXP};
