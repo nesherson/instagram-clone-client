8024b089fb31c10dbd4fc4cf30b5a77548cdfca3
nesherson <nesad.omeragich27@gmail.com>
2021-07-30 22:39:29 +0200

    Refactor: removed redundant code

--- src/features/user/userSlice/userSlice.js
+++ src/features/user/userSlice/userSlice.js
@@ -1,67 +1,25 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { getLocalDateWithOffset } from '../../../util/date';
-
-export const signupUser = createAsyncThunk(
-  'user/signupUser',
-  async ({ email, fullname, username, password }, thunkAPI) => {
+export const fetchUserByUsername = createAsyncThunk(
+  "user/fetchUserByUsername",
+  async (username, thunkAPI) => {
+    const { token } = JSON.parse(localStorage.getItem("userData"));
     try {
-      const response = await fetch('http://localhost:5000/auth/signup', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, fullname, username, password }),
-      });
+      const response = await fetch(
+        `http://localhost:5000/users/user/${username}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+          },
+        }
+      );
 
       let data = await response.json();
 
       if (response.status === 200) {
-        const expirationTime = 3600000;
-        const tokenExpirationDate = new Date(getLocalDateWithOffset().getTime() + expirationTime);
-        const userData = JSON.stringify({
-          userId: data.user.id,
-          token: data.token,
-          tokenExpirationDate: tokenExpirationDate.toISOString()
-        });
-
-        localStorage.setItem('userData', userData);
-        return data;
-      } else {
-        return thunkAPI.rejectWithValue(data);
-      }
-    } catch (err) {
-      return thunkAPI.rejectWithValue(err.response.data);
-    }
-  }
-);
-
-export const loginUser = createAsyncThunk(
-  'user/loginUser',
-  async ({ email, password }, thunkAPI) => {
-    try {
-      const response = await fetch('http://localhost:5000/auth/login', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      let data = await response.json();
-
-      if (response.status === 200) { 
-        const expirationTime = 3600000;
-        const tokenExpirationDate = new Date(getLocalDateWithOffset().getTime() + expirationTime);
-        const userData = JSON.stringify({
-          userId: data.user.id,
-          token: data.token,
-          tokenExpirationDate: tokenExpirationDate.toISOString()
-        });
-
-        localStorage.setItem('userData', userData);
         return data;
       } else {
         return thunkAPI.rejectWithValue(data);
@@ -73,16 +31,16 @@ export const loginUser = createAsyncThunk(
 );
 
 export const fetchUser = createAsyncThunk(
-  'user/fetchUser',
+  "user/fetchUser",
   async (_, thunkAPI) => {
-    const {token} = JSON.parse(localStorage.getItem('userData'));
+    const { token } = JSON.parse(localStorage.getItem("userData"));
     try {
-      const response = await fetch('http://localhost:5000/users/user', {
-        method: 'GET',
+      const response = await fetch(`http://localhost:5000/users/user`, {
+        method: "GET",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'x-auth-token': token,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "x-auth-token": token,
         },
       });
 
@@ -100,19 +58,22 @@ export const fetchUser = createAsyncThunk(
 );
 
 export const savePost = createAsyncThunk(
-  'user/savePost',
-  async ({userId, postId}, thunkAPI) => {
-    const {token} = JSON.parse(localStorage.getItem('userData'));
+  "user/savePost",
+  async ({ userId, postId }, thunkAPI) => {
+    const { token } = JSON.parse(localStorage.getItem("userData"));
     try {
-      const response = await fetch(`http://localhost:5000/users/user/${userId}/save-post`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'x-auth-token': token,
-        },
-        body: JSON.stringify({postId})
-      });
+      const response = await fetch(
+        `http://localhost:5000/users/user/${userId}/save-post`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+          },
+          body: JSON.stringify({ postId }),
+        }
+      );
 
       let data = await response.json();
 
@@ -129,78 +90,23 @@ export const savePost = createAsyncThunk(
 
 const initialState = {
   userId: null,
-  email: '',
-  fullname: '',
-  username: '',
-  profileImg: '',
+  email: "",
+  fullname: "",
+  username: "",
+  profileImg: "",
   posts: [],
   isLoggedIn: false,
   isFetching: false,
   isSuccess: false,
   isError: false,
-  errorMessage: '',
+  errorMessage: "",
 };
 
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
-  reducers: {
-    logoutUser: (state, action) => {
-      localStorage.removeItem('userData');
-      state.userId = null;
-      state.email = '';
-      state.fullname = '';
-      state.username = '';
-      state.profileImg = '';
-      state.posts = [];
-      state.isLoggedIn = false;
-      state.isSuccess = false;
-    }
-  },
+  reducers: {},
   extraReducers: {
-    [signupUser.fulfilled]: (state, action) => {
-      state.isFetching = false;
-      state.isSuccess = true;
-      state.isError = false;
-      state.userId = action.payload.user.id;
-      state.email = action.payload.user.email;
-      state.username = action.payload.user.username;
-      state.fullname = action.payload.user.fullname;
-      state.isLoggedIn = true;
-
-    },
-    [signupUser.pending]: (state, action) => {
-      state.isFetching = true;
-      state.isSuccess = false;
-      state.isError = false;
-    },
-    [signupUser.rejected]: (state, action) => {
-      state.isFetching = false;
-      state.isSuccess = false;
-      state.isError = true;
-      state.errorMessage = action.payload.message;
-    },
-    [loginUser.fulfilled]: (state, action) => {
-      state.isFetching = false;
-      state.isSuccess = true;
-      state.isError = false;
-      state.userId = action.payload.user.id;
-      state.email = action.payload.user.email;
-      state.username = action.payload.user.username;
-      state.fullname = action.payload.user.fullname;
-      state.isLoggedIn = true;
-    },
-    [loginUser.pending]: (state, action) => {
-      state.isFetching = true;
-      state.isSuccess = false;
-      state.isError = false;
-    },
-    [loginUser.rejected]: (state, action) => {
-      state.isFetching = false;
-      state.isSuccess = false;
-      state.isError = true;
-      state.errorMessage = action.payload.message;
-    },
     [fetchUser.fulfilled]: (state, action) => {
       state.isFetching = false;
       state.isSuccess = true;
@@ -215,9 +121,31 @@ const userSlice = createSlice({
     [fetchUser.pending]: (state, action) => {
       state.isFetching = true;
       state.isSuccess = false;
-      state.isError = false
+      state.isError = false;
     },
     [fetchUser.rejected]: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = false;
+      state.isError = true;
+      state.errorMessage = action.payload.message;
+    },
+    [fetchUserByUsername.fulfilled]: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+      state.isError = false;
+      state.userId = action.payload.user.id;
+      state.email = action.payload.user.email;
+      state.username = action.payload.user.username;
+      state.fullname = action.payload.user.fullname;
+      state.profileImg = action.payload.user.profileImg;
+      state.posts = action.payload.user.posts;
+    },
+    [fetchUserByUsername.pending]: (state, action) => {
+      state.isFetching = true;
+      state.isSuccess = false;
+      state.isError = false;
+    },
+    [fetchUserByUsername.rejected]: (state, action) => {
       state.isFetching = false;
       state.isSuccess = false;
       state.isError = true;
@@ -231,7 +159,7 @@ const userSlice = createSlice({
     [savePost.pending]: (state, action) => {
       state.isFetching = true;
       state.isSuccess = false;
-      state.isError = false
+      state.isError = false;
     },
     [savePost.rejected]: (state, action) => {
       state.isFetching = false;
@@ -244,6 +172,6 @@ const userSlice = createSlice({
 
 export const selectUser = (state) => state.user;
 
-export const {logoutUser} = userSlice.actions;
+export const { logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
